f7c18828f7991f9074a2a172c3d83104a0162bfc
longnld <long8a1234@gmail.com>
2022-04-08 12:36:33 +0700

    url change

--- email_uploadapi/urls.py
+++ email_uploadapi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import Email_email_View
 urlpatterns = [
-    path('email_upload', Email_email_View.as_view()),
+    path('infoemail', Email_email_View.as_view()),
 ]

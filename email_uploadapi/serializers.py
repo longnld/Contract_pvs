72c730a124f0a6ceaa4187f21c03822b5e746666
longnld <long8a1234@gmail.com>
2022-04-12 15:40:21 +0700

    fix

--- email_uploadapi/serializers.py
+++ email_uploadapi/serializers.py
@@ -3,4 +3,4 @@ from .models import Email_email
 class Email_email_Serializer(serializers.ModelSerializer):
     class Meta():
         model=Email_email
-        fields=['text_content','attachment_file'] 
+        fields=['text_content'] 
